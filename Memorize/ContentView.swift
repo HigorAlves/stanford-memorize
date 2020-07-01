72bcc724c2579e330dffdca6644af7b3cbadd504
Higor Alves <higorhaalves@gmail.com>
2020-07-01 18:02:55 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Higor Alves on 01/07/20.
+//  Copyright © 2020 Higor Alves Dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
